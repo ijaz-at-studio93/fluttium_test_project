e2449ba9f05a90dd1681e854eecf563395145318
i-am-ijaz <ijazsharif34@gmail.com>
2023-11-27 03:35:53 +0500

    refactor code

---
+++ lib/screens/bottom_nav_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+import 'profile.dart';
+import 'search.dart';
+
+class BottomNavScreen extends StatefulWidget {
+  const BottomNavScreen({super.key});
+
+  @override
+  BottomNavScreenState createState() => BottomNavScreenState();
+}
+
+class BottomNavScreenState extends State<BottomNavScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const SearchPage(),
+    const ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Simple Bottom Nav App'),
+      ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: Material(
+        elevation: 2,
+        child: SizedBox(
+          height: 56, // You can adjust the height according to your design
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildNavItem('Home', 0, Icons.home),
+              _buildNavItem('Search', 1, Icons.search),
+              _buildNavItem('Profile', 2, Icons.person),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onNavItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  Widget _buildNavItem(String label, int index, IconData icon) {
+    return Semantics(
+      label: label,
+      child: InkWell(
+        onTap: () => _onNavItemTapped(index),
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Icon(
+            icon,
+            color: _currentIndex == index
+                ? Theme.of(context).colorScheme.onBackground
+                : Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/profile.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  ProfilePageState createState() => ProfilePageState();
+}
+
+class ProfilePageState extends State<ProfilePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController(text: 'John Doe');
+  final _emailController = TextEditingController(text: 'john.doe@example.com');
+  final _bioController = TextEditingController(text: 'A passionate developer.');
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _bioController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              controller: _nameController,
+              decoration: const InputDecoration(labelText: 'Name'),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter your name';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter your email';
+                } else if (!value.contains('@')) {
+                  return 'Please enter a valid email address';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              controller: _bioController,
+              decoration: const InputDecoration(labelText: 'Bio'),
+              maxLines: 3,
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  // Save the profile changes
+                  // For simplicity, we'll just print the values
+                  print('Name: ${_nameController.text}');
+                  print('Email: ${_emailController.text}');
+                  print('Bio: ${_bioController.text}');
+                }
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
